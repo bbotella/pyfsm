480e5853a1e9406d8ee88ef69006b7fae825fc05
bbotella <contacto@bernardobotella.com>
2015-08-04 18:49:49 +0200

    Sets up files for pypi

---
+++ setupy.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'pyfsm',
+    packages = ['pyfsm'], # this must be the same as the name above
+    version = '0.1',
+    description = 'A random test lib',
+    author = 'Bernardo Botella',
+    author_email = 'contacto@bernardobotella.com',
+    url = 'https://github.com/bbotella/pyfsm', # use the URL to the github repo
+    download_url = 'https://github.com/bbotella/pyfsm/tarball/0.1', # I'll explain this in a second
+    keywords = ['finite state machine', 'fsm'], # arbitrary keywords
+    classifiers = [],
+)
